9760705bd03332c83c4e092adf1685917c6a8142
elenaalegret <elenaalegret@gmail.com>
2026-05-08 16:48:43 +0200

    add: code for noninteractive job

---
+++ run_cluster/run_sam2object.sh
@@ -0,0 +1,12 @@
+
+
+
+
+cd segtrack
+python dataprocess/extract_only_jpg.py
+python dataprocess/get_posed_images.py
+python dataprocess/get_posed_images.py
+
+python seg_tracking.py
+python mask_convert.py
+
